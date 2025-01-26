c3a743ea2db77b1946e5cf115f07075de6be5609
Irfansyah <111201911796@mhs.dinus.ac.id>
2025-01-26 11:01:29 +0700

    feat: add register user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -18,8 +22,30 @@ class AuthController extends Controller
         }
     }
 
-    public function logout(Request $request)
-    {
+    public function register(Request $request) {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required',
+            ]);
+
+            if ($validator->fails()) {
+                return $this->sendError('Validation Error.', $validator->errors(), 422);
+            }
+
+            $input = $request->all();
+            $input['password_encrypt'] = Crypt::encrypt($input["password"]);
+            $input['password'] = bcrypt($input['password']);
+            $user = User::create($input);
+
+            return $this->sendResponse($user, 'User register successfully.');
+        } catch (Exception $e) {
+            return $this->sendError('Internal Error.', ['error' => $e->getMessage()]);
+        }
+    }
+
+    public function logout(Request $request) {
         try {
             $request->user()->tokens()->delete();
             $this->sendResponse(null, 'successfully logged out');
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
     // Location
